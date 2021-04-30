58f1aed486cadce8e5baf2d329742dc1907f5453
Gary Huang <ttiverson3@gmail.com>
2021-05-01 00:23:33 +0800

    create_table.py

--- data/create_table.py
+++ data/create_table.py
@@ -12,17 +12,17 @@ def create_table():
         auth_plugin = "mysql_native_password"
     )
     cursor = mydb.cursor()
-    # cursor.execute("""CREATE TABLE attraction 
-    # (id INT AUTO_INCREMENT PRIMARY KEY, 
-    # name VARCHAR(255), 
-    # category VARCHAR(255),
-    # description VARCHAR(5000),
-    # address VARCHAR(255),
-    # transport VARCHAR(5000),
-    # mrt VARCHAR(255),
-    # latitude float,
-    # longitude float
-    # )""")
+    cursor.execute("""CREATE TABLE attraction 
+    (id INT AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(255), 
+    category VARCHAR(255),
+    description VARCHAR(5000),
+    address VARCHAR(255),
+    transport VARCHAR(5000),
+    mrt VARCHAR(255),
+    latitude float,
+    longitude float
+    )""")
     cursor.execute("""CREATE TABLE image 
     (img_id INT AUTO_INCREMENT PRIMARY KEY, 
     url VARCHAR(255), 
