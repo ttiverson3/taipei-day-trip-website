6a99c1075bb5fccbd100f3c92ec6eb520cbe1887
Gary Huang <ttiverson3@gmail.com>
2021-05-02 17:58:49 +0800

    dbconf.py

--- dbconf.py
+++ dbconf.py
@@ -20,13 +20,20 @@ class Connect:
                 pool_reset_session = True,
                 **self.dbconfig
             )
-            self.cnx = self.cnxpool.get_connection()
-            self.cur = self.cnx.cursor(buffered = True)
-            print("mysql connect success!")
+            # self.cnx = self.cnxpool.get_connection()
+            # self.cur = self.cnx.cursor(buffered = True)
+            print("MySQL connect success!")
         except Exception as e:
-            print("mysql connect error!", e)
+            print("MySQL connect error!", e)
 
     def query(self, sql):
+        self.cnx = self.cnxpool.get_connection()
+        self.cur = self.cnx.cursor(buffered = True)
+        
         self.cur.execute(sql)
         self.result = self.cur.fetchall()
+        
+        self.cur.close()
+        self.cnx.close()
+        # print("MySQL connection is closed")
         return self.result
