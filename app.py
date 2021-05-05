42a0fb5845545f9c51849fefabc44259dd5dd5bd
Gary Huang <ttiverson3@gmail.com>
2021-05-05 17:02:10 +0800

    developed attraction page

--- app.py
+++ app.py
@@ -127,5 +127,5 @@ def attractionId_api(attractionId):
 
 
 if __name__ == "__main__":
-	# app.run(host = "0.0.0.0", port = 3000)
-	app.run(debug = True, port = 3000)
+	app.run(host = "0.0.0.0", port = 3000)
+	# app.run(debug = True, port = 3000)
